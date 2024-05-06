1e5eca7b5acc28ec401d5e5098c958fa3fcfa697
Brumassio <dibrunspeed@gmail.com>
2024-05-06 19:06:59 -0300

    Sessão 5 finalizada

---
+++ Sessao5/Sessao5/ContaBancaria.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sessao5 {
+    internal class ContaBancaria {
+        public int NumeroDaConta { get; private set; }
+        public string _nome;
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numeroDaconta, string nome) {
+            NumeroDaConta = numeroDaconta;
+            _nome = nome;
+        }
+
+        public ContaBancaria(int numeroDaconta, string nome,  double depositoInicial) : this(numeroDaconta,nome){ // o this aproveita a estrutura do outro construtor
+            Deposito(depositoInicial);
+        }
+
+        public string Nome{
+            get{
+                return _nome;
+            }
+            set {
+                if (value != null && value.Length > 2)
+                {
+                    _nome = value;
+                }
+            }
+        }
+        
+        public void Deposito(double valor) {
+            Saldo += valor;
+        }
+
+        public void Saque(double valor) {
+            Saldo -= valor;
+            Saldo -= 5;
+        }
+
+        public override string ToString() {
+            return "Conta: "+ NumeroDaConta +", Titular: "+_nome+", Saldo: "+Saldo.ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Sessao5/Sessao5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace Sessao5 {
+    internal class Program {
+        static void Main(string[] args) {
+            ContaBancaria b1;
+            Console.WriteLine("Exercício Construtores-this-sobrecarga-encapsulamento\n");
+            Console.WriteLine("Entre o número da conta: ");
+            int numCont = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre o titular da conta: ");
+            string Nome = Console.ReadLine();
+            Console.WriteLine("Haverá depósito inicial (s/n)?");
+            char aux = char.Parse(Console.ReadLine());
+            if(aux == 's') {
+                Console.WriteLine("Digite o valor do depósito inicial :");
+                double depInicial = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                b1 = new ContaBancaria(numCont, Nome,depInicial);
+            }
+            else {
+                b1 = new ContaBancaria(numCont, Nome);
+            }
+            Console.WriteLine(b1);
+            Console.WriteLine("Digite o valor do depósito: ");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b1.Deposito(valor);
+            Console.WriteLine(b1);
+
+            Console.WriteLine("Digite o valor do saque: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b1.Saque(valor);
+            Console.WriteLine(b1);
+        }
+    }
+}
