3a865636f12872d5cb14d368347fcbcf95ff012f
Brumassio <dibrunspeed@gmail.com>
2024-05-27 16:22:12 -0300

    Tratamento de Exceções

---
+++ Sessao11/Sessao11/Entities/Account.cs
@@ -0,0 +1,52 @@
+﻿using Sessao11.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sessao11.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; private set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number=number;
+            Holder=holder;
+            Balance=balance;
+            WithdrawLimit=withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            if (WithdrawLimit < amount)
+            {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+            else if (Balance - amount < 0)
+            {
+                throw new DomainException("Not enough balance");
+            }
+
+
+            Balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            return "Number: "+Number+"\nHolder: "+ Holder+"\nBalance: $ "+Balance.ToString("F2",CultureInfo.InvariantCulture)+"\nWithdrawLimit: $ "+ WithdrawLimit.ToString("F2",CultureInfo.InvariantCulture)+ "\n";
+        }
+    }
+}
---
+++ Sessao11/Sessao11/Program.cs
@@ -0,0 +1,49 @@
+﻿using Sessao11.Entities;
+using Sessao11.Entities.Exceptions;
+using System.Globalization;
+
+namespace Sessao11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Exercício de fixação de tratamento de Exceções ");
+                Console.WriteLine("------------------------------------------------------------");
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double limit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Account acc = new Account(number,holder,balance,limit);
+
+                Console.WriteLine();
+                Console.WriteLine(acc);
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                acc.Withdraw(amount);
+                Console.WriteLine("New balance: "+ acc.Balance);
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine("Withdraw error: "+ e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: "+ e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Unexpected error:" + e.Message);
+            }
+
+        }
+    }
+}
